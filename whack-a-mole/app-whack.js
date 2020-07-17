a2dd5f922c98590eab33c3713da66ee5b03d531d
olaftarlowski <olaf.tarlowski@gmail.com>
2020-07-17 13:12:32 +0200

    app generates variables and assigns required class'mole' randomly

--- whack-a-mole/app-whack.js
+++ whack-a-mole/app-whack.js
@@ -1,1 +1,18 @@
-console.log('workin g');
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeleft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove("mole");
+    }) 
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add("mole");
+
+    // assign the id of the randomPosition to hitPosition
+};
+
+randomSquare()
