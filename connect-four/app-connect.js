4817e6e7430ccd4bdb1e65ce4c083240b07da2bb
olaftarlowski <olaf.tarlowski@gmail.com>
2020-07-21 13:21:00 +0200

    basic logic of adding markers implemented

--- connect-four/app-connect.js
+++ connect-four/app-connect.js
@@ -1,1 +1,31 @@
-console.log("connected");
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.section-gameUI__content div');
+    const result = document.querySelector('#result');
+    const displayCurrentPlayer = document.querySelector('#current-player');
+    let currentPlayer = 1;
+
+    for (var i = 0, len = squares.length; i < len; i++ ) {
+        
+        (function(index) {
+            //add an onclick to each square
+            squares[i].onclick = function() {
+                // if the squre below your current square is taken, you can go ontop of it
+                if (squares[index+7].classList.contains('taken')) {
+                    if (currentPlayer === 1) {
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-one');
+                        // change the player
+                        currentPlayer = 2;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    } else if (currentPlayer === 2){
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-two');
+                        //change the player 
+                        currentPlayer = 1;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    } else alert('cant go here');
+                }
+            }
+        })(i)
+    }
+});
